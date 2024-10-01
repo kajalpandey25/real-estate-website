613232ca220cdf6c1f1d8db57d86e52464da3873
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-10-01 16:39:23 +0530

    done

--- client/src/components/Testimonials/Testimonials.jsx
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     <div className="testimonial-section">
       <h2>TESTIMONIAL</h2>
       <p>Featured Residential Projects Across India</p>
-      <p className="testimonial-intro" style={{marginLeft:"400px", color:"black"}}>
+      <p className="testimonial-intro">
         When the age of a person exceeds 50 years, it becomes very important to take care of one’s health as she enters the senior citizen age. With age, the metabolism and functioning of the body become slow.
       </p>
 
