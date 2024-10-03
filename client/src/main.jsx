8f541772b7ac35b8bf2ee81f78bb5b72cd3be301
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-10-03 14:52:50 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Auth0Provider
      domain="dev-unwib2uznmp1ymj4.us.auth0.com"
      clientId="nxnI7UmTSQ0f8V5ZuoFxMwdot43ryb45"
+    //  domain="dev-usn1h41sxibnaiah.us.auth0.com"
+    // clientId="Al5n7IfPXb3diF1feaxtS6RNsW52Ws2L"
      authorizationParams={{
       redirect_uri: "http://localhost:5173/"
      }}
@@ -19,4 +21,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Auth0Provider>
   </React.StrictMode>
 );
-
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 
 export const api = axios.create({
   baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+ 
 });
 
 
